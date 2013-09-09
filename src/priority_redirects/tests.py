3a179c7a3ccf12d73dfe99888c447143fc1d42b5
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-09-09 14:32:53 -0500

    Fixed more of those pointers.

--- src/priority_redirects/tests.py
+++ src/priority_redirects/tests.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 from django.test.utils import override_settings
 from django.utils import six
 
-from .middleware import RedirectFallbackMiddleware
-from .models import Redirect
+from middleware import RedirectFallbackMiddleware
+from models import Redirect
 
 
 @override_settings(
